19a42e17be55f778ad32a1567b953085cfaa2747
benjamincristina <benjamincristina@icloud.com>
2025-09-05 03:00:19 +0200

    More Updated and added JS file for popups.

---
+++ index.js
@@ -0,0 +1,17 @@
+document.querySelector("#music-production-link").addEventListener("click", function() {
+    alert("This page is currently under construction. \n" +
+        "In the meantime, you will be redirected to my YouTube profile. \n" +
+        "Thank you for your patience.");
+});
+
+document.querySelector("#software-development-link").addEventListener("click", function() {
+    alert("This page is currently under construction. \n" +
+        "In the meantime, you will be redirected to my GitHub profile. \n" +
+        "Thank you for your patience.");
+});
+
+document.querySelector("#contact-link").addEventListener("click", function() {
+    alert("This page is currently under construction. \n" +
+        "In the meantime, you will be redirected to your mail application. \n" +
+        "Thank you for your patience.");
+});
